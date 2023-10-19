8763bdfcc0ea1e068ca618d8a7932fdd50e85c14
golden-state0fmind <areyes2009@yahoo.com>
2023-10-18 18:57:47 -0700

    removing homepage and building starter navbar for the layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,34 +4,7 @@ export default function Home() {
   return (
     <div className="flex h-screen bg-gray-50 text-black">
       <div className="w-screen h-screen flex flex-col justify-center items-center">
-        <div className="text-center max-w-screen-sm mb-10">
-          <h1 className="font-bold text-2xl">
-            Happy Hound Care 🐶
-          </h1>
-          <div className="flex justify-between">
-            {/* <Link
-              href="/protected"
-              prefetch={false}
-              className="underline hover:text-stone-400 transition-all"
-            >
-              Protected Routes
-            </Link> */}
-            <Link
-              href="/register"
-              prefetch={false}
-              className="underline hover:text-stone-400 transition-all"
-            >
-              Create Account
-            </Link>
-            <Link
-              href="/login"
-              prefetch={false}
-              className="underline hover:text-stone-400 transition-all"
-            >
-              Sign In
-            </Link>
-          </div>
-        </div>
+        Home Page
       </div>
     </div>
   );
