6099bd8c6568d97477b341987b9baa79d08980de
golden-state0fmind <areyes2009@yahoo.com>
2023-10-18 21:40:52 -0700

    adds mobile nav as hamburger menu

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,9 +1,16 @@
+"use client"
 import Link from "next/link"
+import { useState } from "react";
 
 export const NavBar = () => {
+    const [isMobileOpen, setIsMobileOpen] = useState<boolean>(false)
+
+    const toggleMobileNav = () => { 
+        setIsMobileOpen(!isMobileOpen)
+    }
 
     return (
-        <div className="flex justify-between p-5">
+        <div className="flex justify-between items-center p-5">
             <Link
                 href="/"
                 prefetch={false}
@@ -12,7 +19,7 @@ export const NavBar = () => {
                     Happy Hound Care 🐶
                 </h1>
             </Link>
-            <div>
+            <div className="hidden lg:block">
                 {/* <Link
                 href="/protected"
                 prefetch={false}
@@ -35,6 +42,37 @@ export const NavBar = () => {
                     Sign In
                 </Link>
             </div>
+            {/* START MOBILE NAV */}
+            <div className="lg:hidden">
+                <button className="absolute transform -translate-y-1/2 right-5" onClick={toggleMobileNav} >
+                    <div className="relative">
+                        <div className={isMobileOpen ? `h-1 w-8 bg-gray-700 transform origin-center transition-transform ease-in-out duration-300 rotate-45 translate-y-2` : `h-1 w-8 bg-gray-700`}></div>
+                        <div className={isMobileOpen ? `opacity-0` : `h-1 w-8 my-1 bg-gray-700`}></div> 
+                        <div className={isMobileOpen ? `h-1 w-8 bg-gray-700 transform origin-center transition-transform ease-in-out duration-300 -rotate-45 translate-y-1` : `h-1 w-8 bg-gray-700`}></div> 
+                    </div>
+                </button>
+                <div className={isMobileOpen ? "relative block text-left mt-4" : "hidden"}>
+                    <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 ">
+                        <div className="block px-4 py-2">
+                            <Link
+                                href="/register"
+                                prefetch={false}
+                                className="underline hover:text-stone-400 transition-all block px-4 py-2"
+                            >
+                                Create Account
+                            </Link>
+                            <Link
+                                href="/login"
+                                prefetch={false}
+                                className="underline hover:text-stone-400 transition-all block px-4 py-2"
+                            >
+                                Sign In
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* END MOBILE NAV */}
         </div>
     );
 }
