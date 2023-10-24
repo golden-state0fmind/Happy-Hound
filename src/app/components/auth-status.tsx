70fda35e91c956398009f433f8d4e9e118e289e3
golden-state0fmind <areyes2009@yahoo.com>
2023-10-23 22:46:33 -0700

    starts working in SessionProvider

--- src/app/components/auth-status.tsx
+++ src/app/components/auth-status.tsx
@@ -1,3 +1,4 @@
+import { useSession } from "next-auth/react";
 import { getServerSession } from "next-auth/next";
 
 export default async function AuthStatus() {
@@ -5,10 +6,16 @@ export default async function AuthStatus() {
     return (
         <div className="absolute top-5 w-full flex justify-center items-center">
             {session && (
-                <p className="text-stone-200 text-sm">
+                <p className="text-black text-sm">
                     Signed in as {session.user?.email}
                 </p>
             )}
         </div>
     );
+}
+
+export function useIsUserSignedIn() {
+    const { data: session } = useSession();
+
+    return session !== null;
 }
