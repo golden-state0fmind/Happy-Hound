5db2a50639b88b93f97b480ffafe511725bd5c1d
golden-state0fmind <areyes2009@yahoo.com>
2023-10-23 22:47:13 -0700

    brings in useSession to check user signed in

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,12 +1,16 @@
 "use client"
 import Link from "next/link"
 import { useState } from "react";
+import SignOut from "./sign-out";
+import { useSession } from "next-auth/react";
 
 export const NavBar = () => {
-    const [isMobileOpen, setIsMobileOpen] = useState<boolean>(false)
+    const [isMobileOpen, setIsMobileOpen] = useState<boolean>(false);
+    // TODO: add SessionProvider
+    //const {data: session} = useSession()
 
-    const toggleMobileNav = () => { 
-        setIsMobileOpen(!isMobileOpen)
+    const toggleMobileNav = () => {
+        setIsMobileOpen(!isMobileOpen);
     }
 
     return (
@@ -47,27 +51,34 @@ export const NavBar = () => {
                 <button className="absolute transform -translate-y-1/2 right-5" onClick={toggleMobileNav} >
                     <div className="relative">
                         <div className={isMobileOpen ? `h-1 w-8 bg-gray-700 transform origin-center transition-transform ease-in-out duration-300 rotate-45 translate-y-2` : `h-1 w-8 bg-gray-700`}></div>
-                        <div className={isMobileOpen ? `opacity-0` : `h-1 w-8 my-1 bg-gray-700`}></div> 
-                        <div className={isMobileOpen ? `h-1 w-8 bg-gray-700 transform origin-center transition-transform ease-in-out duration-300 -rotate-45 translate-y-1` : `h-1 w-8 bg-gray-700`}></div> 
+                        <div className={isMobileOpen ? `opacity-0` : `h-1 w-8 my-1 bg-gray-700`}></div>
+                        <div className={isMobileOpen ? `h-1 w-8 bg-gray-700 transform origin-center transition-transform ease-in-out duration-300 -rotate-45 translate-y-1` : `h-1 w-8 bg-gray-700`}></div>
                     </div>
                 </button>
                 <div className={isMobileOpen ? "relative block text-left mt-4" : "hidden"}>
                     <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 ">
                         <div className="block px-4 py-2">
-                            <Link
-                                href="/register"
-                                prefetch={false}
-                                className="underline hover:text-stone-400 transition-all block px-4 py-2"
-                            >
-                                Create Account
-                            </Link>
-                            <Link
-                                href="/login"
-                                prefetch={false}
-                                className="underline hover:text-stone-400 transition-all block px-4 py-2"
-                            >
-                                Sign In
-                            </Link>
+                            {
+                                false
+                                    ? <SignOut />
+                                    :
+                                    <>
+                                        <Link
+                                            href="/register"
+                                            prefetch={false}
+                                            className="underline hover:text-stone-400 transition-all block px-4 py-2"
+                                        >
+                                            Create Account
+                                        </Link>
+                                        <Link
+                                            href="/login"
+                                            prefetch={false}
+                                            className="underline hover:text-stone-400 transition-all block px-4 py-2"
+                                        >
+                                            Sign In
+                                        </Link>
+                                    </>
+                            }
                         </div>
                     </div>
                 </div>
