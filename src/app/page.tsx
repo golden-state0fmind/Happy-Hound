f1321eac0df077093d8a52bef9b1b19740dcc865
golden-state0fmind <areyes2009@yahoo.com>
2023-10-25 15:39:42 -0700

    adds happy dog image as background image to the home page and provides ServiceList component as a client component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
-import Link from "next/link";
+import ServicesList from "./components/service-list";
 
 export default function Home() {
+  const backgroundImageStyle = {
+    backgroundImage: `url('/happy_dog.jpg')`,
+    backgroundSize: 'cover', 
+    backgroundPosition: 'center center',
+  };
   return (
     <div className="flex h-screen bg-gray-50 text-black">
-      <div className="w-screen h-screen flex flex-col justify-center items-center">
-        Home Page
+      <div style={backgroundImageStyle} className="w-screen h-screen flex flex-row">
+        <div className="w-full md:w-1/2 min-w-[20rem] p-6 flex flex-col items-center justify-center">
+          <div className="bg-white p-6 rounded-lg shadow-md">
+            <h1 className="text-base md:text-3xl lg:text-4xl font-semibold">Embrace pet care right in your local community. Reserve reliable pet sitters and dog walkers.</h1> 
+            <ServicesList />
+          </div>
+        </div>
+        <div className="w-1/2"></div>
       </div>
     </div>
   );
