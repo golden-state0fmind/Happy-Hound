47f0051d63b617a287734892972ac39699f0de8d
golden-state0fmind <areyes2009@yahoo.com>
2023-10-31 23:01:21 -0700

    adds request headers to middlewear and whitelist routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,23 +2,42 @@ import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
 export default async function middleware(req: NextRequest) {
-    // Get the pathname of the request (e.g. /, /protected)
+    const nonce = Buffer.from(crypto.randomUUID()).toString('base64');
     const path = req.nextUrl.pathname;
-    
-    // If it's the root path, just render it
-    if (path === "/") {
+
+    const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+    style-src 'self' 'nonce-${nonce}';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    block-all-mixed-content;
+    upgrade-insecure-requests;
+  `.replace(/\s{2,}/g, ' ').trim(); // Cleaning CSP string
+
+    const requestHeaders = new Headers(req.headers);
+    requestHeaders.set('x-nonce', nonce);
+    requestHeaders.set('Content-Security-Policy', cspHeader);
+
+    const whitelistedRoutes = ['/', '/dogsitter', '/login', '/register'];
+    if (whitelistedRoutes.includes(path)) {
         return NextResponse.next();
     }
 
-    const session = await getToken({
-        req,
-        secret: process.env.NEXTAUTH_SECRET,
-    });
+    const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
-    if (!session && path === "/protected") {
-        return NextResponse.redirect(new URL("/login", req.url));
-    } else if (session && (path === "/login" || path === "/register")) {
-        return NextResponse.redirect(new URL("/protected", req.url));
+    if (!session && path === '/protected') {
+        return NextResponse.redirect(new URL('/login', req.url));
+    } else if (session && (path === '/login' || path === '/register')) {
+        return NextResponse.redirect(new URL('/protected', req.url));
     }
-    return NextResponse.next();
+
+    return NextResponse.next({
+        headers: requestHeaders,
+        request: { headers: requestHeaders },
+    });
 }
