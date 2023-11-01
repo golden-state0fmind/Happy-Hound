37f86fdff822da15ed71616ed94d489a022ca428
golden-state0fmind <areyes2009@yahoo.com>
2023-10-31 23:05:42 -0700

    starts adding headers to whole app for inline styles and nonce

--- next.config.js
+++ next.config.js
@@ -1,4 +1,54 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    // reactStrictMode: true,
+    // swcMinify: true,
+    // async headers() {
+    //     const nonce = Buffer.from(crypto.randomUUID()).toString('base64');
 
-module.exports = nextConfig
+    //     const cspHeader = `
+    //         default-src 'self';
+    //         script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+    //         style-src 'self' 'nonce-${nonce}';
+    //         img-src 'self' blob: data:;
+    //         font-src 'self';
+    //         object-src 'none';
+    //         base-uri 'self';
+    //         form-action 'self';
+    //         frame-ancestors 'none';
+    //         block-all-mixed-content;
+    //         upgrade-insecure-requests;
+    //         `
+    //         .replace(/\s{2,}/g, ' ')
+    //         .trim(); // Cleaning CSP string
+
+    //     return [
+    //         {
+    //             source: '/(.*)',
+    //             headers: [
+    //                 {
+    //                     key: 'X-Frame-Options',
+    //                     value: 'DENY',
+    //                 },
+    //                 {
+    //                     key: 'Content-Security-Policy',
+    //                     value: cspHeader,
+    //                 },
+    //                 {
+    //                     key: 'X-Content-Type-Options',
+    //                     value: 'nosniff',
+    //                 },
+    //                 {
+    //                     key: 'Permissions-Policy',
+    //                     value: "camera=(); battery=(self); geolocation=(); microphone=('https://a-domain.com')",
+    //                 },
+    //                 {
+    //                     key: 'Referrer-Policy',
+    //                     value: 'origin-when-cross-origin',
+    //                 },
+    //             ],
+    //         },
+    //     ];
+    // },
+};
+
+module.exports = nextConfig;
