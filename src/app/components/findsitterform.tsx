4b6ba8d9d9007f024fecf5340bd7f94bad0bc740
golden-state0fmind <areyes2009@yahoo.com>
2023-11-02 14:19:01 -0700

    fixes user input for number of dogs, fixes zipcode input allowing user to enter only five numbers and fixes user calendar having the default value for the current day

--- src/app/components/findsitterform.tsx
+++ src/app/components/findsitterform.tsx
@@ -1,32 +1,80 @@
-import { useState } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import { RootState } from '../reducers';
 import { useSelector } from 'react-redux';
 import Link from 'next/link';
 import LoadingDots from './loading-dots';
 
 const FindSitterForm = () => {
+    const [zipCode, setZipCode] = useState('');
     const [loading, setLoading] = useState(false);
-    const [numberOfDogs, setNumberOfDogs] = useState(1);
+    const [numberOfDogs, setNumberOfDogs] = useState<number | string>(1);
     const serviceState = useSelector((state: RootState) => state.service);
     const { dropInVisits, dogWalking, houseSitting } = serviceState;
 
+    // Adding all selected services into an array then placing the elements as values into the input field as read only
+    // User can click link in label to select more services
     const servicesArray = [];
     if (dropInVisits) servicesArray.push('Drop-in Visits');
     if (dogWalking) servicesArray.push('Dog Walking');
     if (houseSitting) servicesArray.push('House Sitting');
     const servicesText = servicesArray.join(', ');
 
-    const handleInputChange = (e: any) => {
-        const value = parseInt(e.target.value, 10);
-        if (!isNaN(value) && value >= 1 && value <= 10) {
-            setNumberOfDogs(value);
+    const handleDogNumber = (e: ChangeEvent<HTMLInputElement>) => {
+        const dogNumber = Number(e.target.value)
+        if (isNaN(dogNumber)) {
+            setNumberOfDogs(1)
+        }
+        else if (dogNumber > 10) {
+            setNumberOfDogs(10);
+        } else {
+            setNumberOfDogs(e.target.value)
         }
     };
 
-    const handleSubmit = (e: any) => {
+    const isValidZipCode = (value: string) => {
+        const zipCodePattern = /^\d{5}(-\d{4})?$/;
+        const isValid = zipCodePattern.test(value);
+        if (!isValid) {
+            setLoading(false);
+            alert('Please enter a valid ZIP code');
+        }
+    }
+
+    // TODO: Format the zipcode to match 12345-4321 -> this format can be used to be more precise as areaCode-address 
+    // Currently only matches format 12345 -> areaCode
+    function formatZipCode(areaCode: string) {
+        // Remove non-numeric characters and hyphens
+        const cleanZip = areaCode.replace(/[^\d-]/g, '');
+        // Format ZIP code
+        if (cleanZip.length <= 5) {
+            return cleanZip.replace(/^(\d{5}).*/, '$1');
+        } else {
+            return cleanZip.replace(/^(\d{5})(\d{0,4}).*/, '$1-$2');
+        }
+    }
+
+    const handleZipCodeInput = (e: any) => {
+        const value = e.target.value
+        const formattedZip = formatZipCode(value)
+        setZipCode(formattedZip)
+    }
+
+    const today = new Date();
+    const day = today.getDate().toString().padStart(2, '0'); // Get the day (1-31) and pad with zero if needed
+    const month = (today.getMonth() + 1).toString().padStart(2, '0'); // Get the month (0-11); +1 to make it 1-12 and pad with zero if needed
+    const year = today.getFullYear(); // Get the year (e.g., 2023)
+
+    // Provided date in MM/DD/YYYY format
+    const providedDate = `${month}/${day}/${year}`;
+
+    // Convert to YYYY-MM-DD format
+    const parts = providedDate.split('/');
+    const formattedDate = `${parts[2]}-${parts[0].padStart(2, '0')}-${parts[1].padStart(2, '0')}`;
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         setLoading(true);
-        // Your form submission logic here
+        isValidZipCode(zipCode);
     };
 
     return (
@@ -40,13 +88,14 @@ const FindSitterForm = () => {
                 <input
                     id="pets"
                     name="pets"
-                    type="number"
+                    type="text"
                     required
                     pattern="\d*"
                     inputMode="numeric"
+                    maxLength={2}
                     value={numberOfDogs}
-                    onChange={handleInputChange}
-                    className="mt-1 block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
+                    onChange={handleDogNumber}
+                    className="mt-1 block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-green-700 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
                 />
             </div>
             <div>
@@ -62,26 +111,30 @@ const FindSitterForm = () => {
                     readOnly
                     disabled
                     value={servicesText}
-                    className="mt-1 block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
+                    className="mt-1 block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-green-700 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
                 />
             </div>
 
             <div>
                 <label htmlFor="location" className="block text-xs text-gray-600 uppercase">
-                    Location near
+                    Sitters near
                 </label>
                 <input
                     id="location"
                     name="location"
                     type="text"
                     required
-                    className="mt-1 block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
+                    value={zipCode}
+                    maxLength={5}
+                    onChange={handleZipCodeInput}
+                    className="mt-1 block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-green-700 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
                 />
             </div>
 
             <div>
                 <label htmlFor="startDate" className="block text-xs text-gray-600 uppercase">
                     For what days?
+                    <span className="ps-2 text-xs text-blue-700">Add dates to see available sitters</span>
                 </label>
                 <div className="mt-1 grid grid-cols-2 gap-2">
                     <input
@@ -90,6 +143,7 @@ const FindSitterForm = () => {
                         name="startDate"
                         type="date"
                         required
+                        defaultValue={formattedDate}
                         className="appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
                     />
                     <input
@@ -98,6 +152,7 @@ const FindSitterForm = () => {
                         name="endDate"
                         type="date"
                         required
+                        defaultValue={formattedDate}
                         className="appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
                     />
                 </div>
