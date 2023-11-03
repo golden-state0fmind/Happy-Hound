24df32dbc32966d0aae1c875d745c4dcbe48304c
golden-state0fmind <areyes2009@yahoo.com>
2023-11-02 18:23:18 -0700

    changes svg checkmark for html checkmark now allowing checkmarks to render on mobile devices

--- src/app/components/service-list.tsx
+++ src/app/components/service-list.tsx
@@ -103,20 +103,7 @@ const ServiceCard = ({ title, checked, onChange }: ServiceCardProps) => {
         >
             <div className={`w-6 h-6 mr-2 border-2 ${checked ? 'border-green-600' : 'border-blue-800'} rounded-full flex items-center justify-center`}>
                 {checked && (
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="green"
-                        className="checkmark"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M5 13l4 4L19 7"
-                        />
-                    </svg>
+                    <div className='text-base md:text-lg lg:text-xl text-green-600' > &#x2713; </div>
                 )}
             </div>
             <h2 className={`text-base md:text-lg lg:text-xl font-semibold ${checked ? 'text-green-800' : ''}`}>{title}</h2>
