f036b7ce26bbf52b96517772ac7eca4ff74a459a
golden-state0fmind <areyes2009@yahoo.com>
2023-11-03 11:05:34 -0700

    updates middleware route name from protected to profile

--- src/middleware.ts
+++ src/middleware.ts
@@ -30,10 +30,10 @@ export default async function middleware(req: NextRequest) {
 
     const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
-    if (!session && path === '/protected') {
+    if (!session && path === '/profile') {
         return NextResponse.redirect(new URL('/login', req.url));
     } else if (session && (path === '/login' || path === '/register')) {
-        return NextResponse.redirect(new URL('/protected', req.url));
+        return NextResponse.redirect(new URL('/profile', req.url));
     }
 
     return NextResponse.next({
