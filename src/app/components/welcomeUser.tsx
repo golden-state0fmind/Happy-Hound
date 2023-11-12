696a66d42413391a46f85a7be2e41543f04f94ea
golden-state0fmind <areyes2009@yahoo.com>
2023-11-12 12:42:54 -0700

    brings in rootstate from redux for the current user

---
+++ src/app/components/welcomeUser.tsx
@@ -0,0 +1,41 @@
+"use client"
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Link from 'next/link';
+import { RootState } from '../reducers';
+import { fetchUser, setCurrentUser } from '../reducers/userReducer';
+
+
+export default function WelcomeUser() {
+    const user = useSelector((state: RootState) => state.user);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        fetchUser()
+            .then((res) => {
+                let userData = res.data;
+                let currentUser = {
+                    id: userData.id,
+                    email: userData.email,
+                    firstName: userData.firstName,
+                    lastName: userData.lastName,
+                    role: userData.role
+                }
+                dispatch(setCurrentUser(currentUser));
+            })
+            .catch((error) => {
+                // Handle error if needed
+                console.error("Error:", error);
+            });
+    }, []);
+
+    return (
+        <div className="bg-gray-100 rounded-lg shadow-md px-4 py-8 mb-4">
+            {/* Placeholder content */}
+            <h2 className='text-base md:text-lg lg:text-xl font-semibold text-blue-800' >Welcome {user.firstName}</h2>
+            <Link className="underline text-base md:text-lg lg:text-xl hover:text-blue-600 transition-all block py-2" href="/profile">
+                Edit Profile
+            </Link>
+        </div>
+    )
+}
