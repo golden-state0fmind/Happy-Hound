f1be65dc0745a1315f05df68393a060c7c0ab4d8
golden-state0fmind <areyes2009@yahoo.com>
2023-11-12 12:40:48 -0700

    adds new user reducer that will update state after successful api call

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import serviceReducer from './serviceReducer';
-import selectSitter from './sitterReducer'
+import selectSitter from './sitterReducer';
+import userReducer from './userReducer';
 
 const rootReducer = combineReducers({
     service: serviceReducer, // Add more reducers as needed
     dogsitter: selectSitter,
+    user: userReducer
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
