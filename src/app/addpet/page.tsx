12b85f426fad7f1788265111ade73a35d4012b7c
golden-state0fmind <areyes2009@yahoo.com>
2023-11-12 20:04:34 -0700

    adds redux provider to get the current user for adding their pet

--- src/app/addpet/page.tsx
+++ src/app/addpet/page.tsx
@@ -1,9 +1,11 @@
+"use client"
+import ReduxProvider from "../components/WithReduxProvider"
 import PetForm from "../components/addpetform"
 
 export default function AddPet() {
     return (
-        <>
+        <ReduxProvider>
             <PetForm />
-        </>
+        </ReduxProvider>
     )
 }
