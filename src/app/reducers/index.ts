e05c354cfd21e956b6acef1823c9b45111a1e01a
golden-state0fmind <areyes2009@yahoo.com>
2023-11-15 13:00:01 -0700

    adds dog reducer and dogListReducer to rootReducer

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -2,11 +2,15 @@ import { combineReducers } from 'redux';
 import serviceReducer from './serviceReducer';
 import selectSitter from './sitterReducer';
 import userReducer from './userReducer';
+import dogReducer from './dogReducer';
+import dogListReducer from './dogListReducer';
 
 const rootReducer = combineReducers({
     service: serviceReducer, // Add more reducers as needed
     dogsitter: selectSitter,
-    user: userReducer
+    user: userReducer,
+    dog: dogReducer,
+    dogList: dogListReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
