c16a219c8c6239cf95fa0f9dd1675c6dc7a76ea8
golden-state0fmind <areyes2009@yahoo.com>
2023-11-21 10:52:17 -0700

    clears session storage after user signs out allowing other users to login within the same browser and not view previous users profile settings

--- src/app/components/sign-out.tsx
+++ src/app/components/sign-out.tsx
@@ -2,13 +2,17 @@
 import { signOut } from "next-auth/react";
 
 export default function SignOut() {
+    const handleSignOut = () => {
+        signOut()
+        sessionStorage.clear();
+    }
     return (
         <button
             aria-label="Sign out"
             className="text-red-700 hover:text-red-500 transition-all"
-            onClick={() => signOut()}
+            onClick={handleSignOut}
         >
-            Sign out 
+            Sign out
         </button>
     );
 }
