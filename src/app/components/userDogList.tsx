dc60e5c478152d40626c7c5495717bd8dae78d23
golden-state0fmind <areyes2009@yahoo.com>
2023-11-21 21:07:20 -0700

    adds loading dots and loading state

--- src/app/components/userDogList.tsx
+++ src/app/components/userDogList.tsx
@@ -1,12 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { RootState } from "../reducers";
 import { DogState, fetchDog } from "../reducers/dogReducer";
 import { useDispatch, useSelector } from 'react-redux';
 import { addDogToList, removeDogById } from "../reducers/dogListReducer";
 import Link from "next/link";
 import toast from "react-hot-toast";
+import LoadingDots from "./loading-dots";
 
 export default function UserDogList() {
+    const [loading, setLoading] = useState(true);
     const dispatch = useDispatch();
     const dogList = useSelector((state: RootState) => state.dogList.dogList);
 
@@ -15,14 +17,23 @@ export default function UserDogList() {
             .then((res: { data: DogState[] }) => {
                 const userDogs = res.data;
                 userDogs.forEach((dog) => {
-                    dispatch(addDogToList(dog))
+                    dispatch(addDogToList(dog));
                 })
             })
             .catch((error: MessageEvent) => {
                 // Handle error if needed
                 console.error("Error:", error);
+                setLoading(true);
             });
     }, []);
+
+    useEffect(() => {
+        if (dogList.length > 0) {
+            setLoading(false);
+        } else {
+            setLoading(true);
+        }
+    }, [dogList])
 
     const handleDeletePet = (id: number, name: string | null) => {
         fetch(`/api/deletepet/${id}`)
@@ -40,27 +51,33 @@ export default function UserDogList() {
 
     return (
         <ul>
-            {dogList.length > 0 ? (
-                dogList.map((dog: { id: number; name: string | null; userId?: number | null; aboutPet?: string | null; additionalInfo?: string | null; adoptionDate?: string | null; aloneTime?: string | null; birthMonth?: string | null; birthYear?: string | null; breed?: string | null; catFriendly?: boolean | null; childFriendly?: boolean | null; dogFriendly?: boolean | null; energyLevel?: boolean | null; feedingSchedule?: string | null; healthInfo?: string | null; houseTrained?: boolean | null; medication?: string | null; microchipped?: boolean | null; photo?: string | null; pottyBreakSchedule?: string | null; sex?: string | null; spayed?: boolean | null; vetAddress?: string | null; vetName?: string | null; vetPhone?: number | null; weight?: string | null; }, index: number) => (
-                    <li key={index}>
-                        <div className='flex justify-between underline text-base md:text-lg lg:text-xl transition-all'>
-                            <Link className='hover:text-blue-600' href={""}>
-                                {(dog.name)}
-                            </Link>
-                            <div className='underline text-base md:text-lg lg:text-xl transition-all'>
-                                <Link className='hover:text-green-700 pe-5' href={`/editpetdetails/${dog.id}`}>
-                                    Edit
-                                </Link>
-                                <span onClick={() => handleDeletePet(dog.id, dog.name)} className='underline text-base md:text-lg lg:text-xl transition-all hover:text-red-700 cursor-pointer' >
-                                    Delete
-                                </span>
-                            </div>
-                        </div>
+            {loading
+                ?
+                (
+                    <li>
+                        <LoadingDots color="blue" />
                     </li>
-                ))
-            ) : (
-                <li>Fetching Dogs...</li>
-            )}
+                )
+                :
+                (
+                    dogList.map((dog: { id: number; name: string | null; userId?: number | null; aboutPet?: string | null; additionalInfo?: string | null; adoptionDate?: string | null; aloneTime?: string | null; birthMonth?: string | null; birthYear?: string | null; breed?: string | null; catFriendly?: boolean | null; childFriendly?: boolean | null; dogFriendly?: boolean | null; energyLevel?: boolean | null; feedingSchedule?: string | null; healthInfo?: string | null; houseTrained?: boolean | null; medication?: string | null; microchipped?: boolean | null; photo?: string | null; pottyBreakSchedule?: string | null; sex?: string | null; spayed?: boolean | null; vetAddress?: string | null; vetName?: string | null; vetPhone?: number | null; weight?: string | null; }, index: number) => (
+                        <li key={index}>
+                            <div className='flex justify-between underline text-base md:text-lg lg:text-xl transition-all'>
+                                <Link className='hover:text-blue-600' href={""}>
+                                    {(dog.name)}
+                                </Link>
+                                <div className='underline text-base md:text-lg lg:text-xl transition-all'>
+                                    <Link className='hover:text-green-700 pe-5' href={`/editpetdetails/${dog.id}`}>
+                                        Edit
+                                    </Link>
+                                    <span onClick={() => handleDeletePet(dog.id, dog.name)} className='underline text-base md:text-lg lg:text-xl transition-all hover:text-red-700 cursor-pointer' >
+                                        Delete
+                                    </span>
+                                </div>
+                            </div>
+                        </li>
+                    )))
+            }
         </ul>
     );
 };
