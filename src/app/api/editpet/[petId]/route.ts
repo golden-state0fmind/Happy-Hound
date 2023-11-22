547241903da338d78cf1d93500e23042528d16e4
golden-state0fmind <areyes2009@yahoo.com>
2023-11-21 21:12:44 -0700

    changes the route handler from patch to put and adds the necessarry data to edit the dog values with

--- src/app/api/editpet/[petId]/route.ts
+++ src/app/api/editpet/[petId]/route.ts
@@ -2,12 +2,40 @@ import { NextResponse } from "next/server";
 import { prisma } from "../../../lib/prisma";
 import { getServerSession } from "next-auth/next";
 
-export async function PATCH(req: Request) {
+export async function PUT(req: Request) {
     const { pathname } = new URL(req.url)
     const lastSlashIndex = pathname.lastIndexOf("/"); // Finds the index of the last occurence of "/"
     const sessionUser = await getServerSession();
     const email = sessionUser?.user?.email;
     const petId = pathname.slice(lastSlashIndex + 1); // slices off "/" to get pet id in pathname
+    const {
+        name,
+        userId,
+        photo,
+        weight,
+        birthMonth,
+        birthYear,
+        sex,
+        breed,
+        microchipped,
+        spayed,
+        houseTrained,
+        childFriendly,
+        dogFriendly,
+        catFriendly,
+        adoptionDate,
+        aboutPet,
+        pottyBreakSchedule,
+        energyLevel,
+        feedingSchedule,
+        aloneTime,
+        medication,
+        additionalInfo,
+        healthInfo,
+        vetName,
+        vetPhone,
+        vetAddress
+    } = await req.json();
 
     if (email && lastSlashIndex !== -1) {
         const updatePet = await prisma.dog.update({
@@ -15,7 +43,32 @@ export async function PATCH(req: Request) {
                 id: Number(petId), // Convert petId to a number
             },
             data: {
-                name: 'changed dog name',
+                name,
+                userId,
+                photo,
+                weight,
+                birthMonth,
+                birthYear,
+                sex,
+                breed,
+                microchipped,
+                spayed,
+                houseTrained,
+                childFriendly,
+                dogFriendly,
+                catFriendly,
+                adoptionDate,
+                aboutPet,
+                pottyBreakSchedule,
+                energyLevel,
+                feedingSchedule,
+                aloneTime,
+                medication,
+                additionalInfo,
+                healthInfo,
+                vetName,
+                vetPhone,
+                vetAddress,
             },
         })
         return NextResponse.json({ data: updatePet, status: 200 });
