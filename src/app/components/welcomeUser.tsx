2c87a606cefbac0263d14f73050fe55b86373ff0
golden-state0fmind <areyes2009@yahoo.com>
2023-11-21 21:07:57 -0700

    adds loading dots for loading users name

--- src/app/components/welcomeUser.tsx
+++ src/app/components/welcomeUser.tsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import Link from 'next/link';
 import { RootState } from '../reducers';
 import { fetchUser, setCurrentUser } from '../reducers/userReducer';
+import LoadingDots from './loading-dots';
 
 
 export default function WelcomeUser() {
@@ -32,10 +33,19 @@ export default function WelcomeUser() {
     return (
         <div className="bg-gray-100 rounded-lg shadow-md px-4 py-8 mb-4">
             {/* Placeholder content */}
-            <h2 className='text-base md:text-lg lg:text-xl font-semibold text-blue-800' >Welcome {user.firstName}</h2>
+            <h2 className='text-base md:text-lg lg:text-xl font-semibold text-blue-800' >
+                <span className='pe-2'>
+                    Welcome
+                </span>
+                {
+                    user.firstName !== null
+                        ? user.firstName
+                        : <LoadingDots color="blue" />
+                }
+            </h2>
             <Link className="underline text-base md:text-lg lg:text-xl hover:text-blue-600 transition-all block py-2" href="/profile">
                 Edit Profile
             </Link>
         </div>
-    )
+    );
 }
