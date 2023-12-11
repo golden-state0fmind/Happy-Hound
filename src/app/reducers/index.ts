efb02928de820bca5ff13430f161c90db5a93bb8
golden-state0fmind <areyes2009@yahoo.com>
2023-12-11 12:46:03 -0700

    adds new dog siiter list reducer for redux

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -4,6 +4,7 @@ import selectSitter from './sitterReducer';
 import userReducer from './userReducer';
 import dogReducer from './dogReducer';
 import dogListReducer from './dogListReducer';
+import dogSitterListReducer from './dogSitterListReducer';
 
 const rootReducer = combineReducers({
     service: serviceReducer, // Add more reducers as needed
@@ -11,6 +12,7 @@ const rootReducer = combineReducers({
     user: userReducer,
     dog: dogReducer,
     dogList: dogListReducer,
+    dogSitterList: dogSitterListReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
